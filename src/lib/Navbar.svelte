3c4b87bdf4925c8d0428dfcf6fd703eca79c9b7c
GeorgeAzma <gg20060705@gmail.com>
2023-08-03 18:19:54 +0400

    Improved Navbar + Copyright/Contact Info

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,36 +1,74 @@
 <nav id="navbar">
-	<a href="shadertoy.com/user/Peace">Shaders</a>
-	<a>Github</a>
-	<a>Fiverr</a>
+	<a href="https://shadertoy.com/user/Peace" target="_blank">
+		<img class="nav-icon" src="shadertoy.png" alt="Shadertoy Icon" />
+		Shaders</a
+	>
+	<a href="https://fiverr.com/lumiey?public_mode=true" target="_blank"
+		><svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400"
+			><circle cx="200" cy="200" fill="#00b22d" r="200" /><g fill="#fff"
+				><path
+					d="M364.4 162.7c0-6.6-5.2-12-11.8-12-6.4 0-11.7 5.3-11.7 12 0 6.6 5.2 12 11.7 12 6.6.1 11.8-5.3 11.8-12zm-11.8 8.7c-4.5 0-8-3.8-8-8.7 0-4.8 3.5-8.6 8-8.6 4.6 0 8.2 3.8 8.2 8.6 0 4.9-3.6 8.7-8.2 8.7z"
+				/><path
+					d="M355.8 163.7c.6-.2 1.9-1.1 1.9-3 0-2.3-1.5-3.7-4-3.7h-5.3v11.3h3.5v-3.8h.9l1.6 3.8h3.8l-2.1-3.9c-.2-.6-.3-.7-.3-.7zm-3-1.6h-.9v-2.7h.9c.8 0 1.2.4 1.2 1.3.1.9-.4 1.4-1.2 1.4z"
+				/><circle cx="104.6" cy="163" r="9" /><path
+					d="M114 177.9H72.8v-2.7c0-5.3 5.3-5.4 8-5.4 3.1 0 4.5.3 4.5.3v-14.6s-2.8-.4-6.6-.4c-8.6 0-24.5 2.4-24.5 20.6v2.3h-7.5v13.5h7.5V220h-7v13.5H81V220h-8.2v-28.5h22.5V220h-7v13.5H121V220h-7zm70 0h-29.5v13.5h5l-6.4 20c-1.2 3.3-1.5 7.3-1.5 7.3h-.4s-.3-4-1.5-7.3l-6.4-20h5v-13.5h-29.5v13.5h6.2l15.4 42h22l15.4-42h6.2zm54.6 25.5c0-15.4-9.3-26.8-25.8-26.8-17.9 0-28.9 12.7-28.9 29 0 14.8 10.7 29.1 30.5 29.1 15 0 23.9-7.8 23.9-7.8l-6.8-12.9s-7.4 5.3-15.6 5.3c-5.9 0-11.5-3.1-12.9-10.2h35.2c-.1-.1.4-3.9.4-5.7zm-35.2-4.6c1-4.3 3.6-8.2 8.9-8.2 4.1 0 7 3.8 7 8.2zm114.1-8.1h-.2s.2-1.1.2-2.8V185c0-5.1-2.8-7.1-7.9-7.1h-17.5v13.5h5.2c1.5 0 2.4.9 2.4 2.4V220h-7.5v13.5h33.7V220h-7.5v-8.1c0-10.1 5-16.7 15.3-16.7 2.3 0 3.8.3 3.8.3v-18.3s-1.1-.2-2.2-.2c-8.4-.1-15.4 6.1-17.8 13.7zm-49.3 0h-.2s.2-1.1.2-2.8V185c0-5.1-2.8-7.1-7.9-7.1h-17.5v13.5h5.2c1.5 0 2.4.9 2.4 2.4V220h-7.5v13.5h33.7V220h-7.5v-8.1c0-10.1 5-16.7 15.3-16.7 2.3 0 3.8.3 3.8.3v-18.3s-1.1-.2-2.2-.2c-8.5-.1-15.5 6.1-17.8 13.7z"
+				/></g
+			></svg
+		>Fiverr</a
+	>
+	<a href="https://github.com/GeorgeAzma" target="_blank">
+		<svg class="nav-icon" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+				fill="#fff"
+			/></svg
+		>
+		Github
+	</a>
 </nav>
+<div />
 
 <style>
 	#navbar {
 		display: flex;
-		position: absolute;
-		justify-content: center;
-		top: 0px;
-		left: 0px;
-		right: 0px;
-		background-color: #ffffff20;
-		border-bottom: 2px solid #ffffff33;
+		text-shadow: -1px 2px 4px black;
 	}
 	#navbar a {
-		/* border: 2px solid black; */
+		display: flex;
+		height: 32px;
 		flex-grow: 1;
-		flex: 1 1 1;
-		text-align: center;
-		border-right: 2px solid #ffffff33;
+		gap: 8px;
+		margin-inline: 4px;
+		align-items: center;
+		justify-content: center;
 		transition: 200ms;
-		padding: 16px;
 		color: #ffffff;
 		text-decoration: none;
+		padding: 16px;
+		-webkit-user-drag: none;
+		box-shadow: inset 0px 0px 48px #ddeeff55, inset 0px 0px 6px #ddeeff55;
+		border-radius: 16px;
 	}
 	#navbar a:last-child {
 		border-right: none;
 	}
 	#navbar a:hover {
-		background-color: #ffffff30;
+		padding-inline: 1.5rem;
+		background-color: #ddeeff55;
+		background-clip: padding-box;
 		transition: 200ms;
+		box-shadow: inset 0px 0px 8px #ddeeff55;
+		text-shadow: 0px 0px 12px #ddeeff;
+	}
+	#navbar a:active {
+		padding-inline: 1.6rem;
+		background-color: #ddeeff30;
+		transition: 150ms;
+		box-shadow: inset 0px 0px 8px #ddeeffff;
+	}
+	.nav-icon {
+		height: 100%;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,18 +14,18 @@
 	onMount(readFile);
 </script>
 
-<title>Portfolio</title>
-<meta name="description" />
-
 <Navbar />
 <Shader {frag} />
 <div class="main-text">
 	I'm George Azmaipharashvili, Software engineer mainly specialized at graphics.
 </div>
 
+<div id="contact-info">Contact: geo.azmai@gmail.com</div>
+<div id="bottom-text">© 2023-2025 George Azmaipharashvili. All rights reserved.</div>
+
 <style>
 	@font-face {
-		font-family: 'Roboto';
+		font-family: 'Roboto Black';
 		src: url('$lib/assets/Roboto/Black.ttf') format('truetype');
 	}
 	.main-text {
@@ -35,11 +35,28 @@
 		font-size: 3rem;
 		font-weight: 800;
 		text-align: center;
-		top: 30%;
+		top: 40%;
 		padding-inline: 10%;
-		transform: translateY(-30%);
-		filter: drop-shadow(0px 0px 3px #221133);
+		transform: translateY(-40%);
+		filter: drop-shadow(0px 0px 4px #221133);
 		overflow: auto;
 		overflow-wrap: break-word;
+	}
+	#bottom-text {
+		font-family: 'Trebuchet MS', sans-serif;
+		font-size: small;
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		padding: 4px;
+		color: #ddddff39;
+	}
+	#contact-info {
+		font-family: 'Trebuchet MS', sans-serif;
+		font-size: small;
+		position: absolute;
+		bottom: 0;
+		padding: 4px;
+		color: #ddddff66;
 	}
 </style>
